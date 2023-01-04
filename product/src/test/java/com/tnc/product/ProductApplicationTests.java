c43e738f6182cd5f738e4027d9b8c4b618b50d51
Tnc <tncbogdan@gmail.com>
2023-01-04 14:18:32 +0200

    add product microservice

---
+++ product/src/test/java/com/tnc/product/ProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tnc.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
