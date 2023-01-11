3f917503fecc53e6b3cdf09cd229a5aacc4e398f
Tnc <tncbogdan@gmail.com>
2023-01-11 11:07:51 +0200

    added eureka dependency in client and product pom

--- order/src/main/java/com/tnc/order/controller/OrderController.java
+++ order/src/main/java/com/tnc/order/controller/OrderController.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.bouncycastle.asn1.ocsp.ResponderID;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,8 +18,8 @@ public class OrderController {
     private final DtoMapperOrder dtoMapperOrder;
     private final OrderService orderService;
 
-    @PostMapping
-    ResponseEntity<OrderDto>createOrder(OrderDto orderDto){
+    @PostMapping(name = "/add")
+    ResponseEntity<OrderDto>createOrder(@RequestBody OrderDto orderDto){
         var order = orderService.createOrder(dtoMapperOrder.toDomain(orderDto));
         return ResponseEntity.ok(dtoMapperOrder.toDto(order));
     }
