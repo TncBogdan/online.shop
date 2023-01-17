c5f8b21c0f9f7c0fb74c8a010f175001c2b16290
Tnc <tncbogdan@gmail.com>
2023-01-17 21:12:31 +0200

    add security in api gateway

--- api.gateway/src/main/java/com/tnc/api/gateway/Application.java
+++ api.gateway/src/main/java/com/tnc/api/gateway/Application.java
@@ -2,21 +2,11 @@ package com.tnc.api.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
-
-	@Bean
-	public RouteLocator myRoutes(RouteLocatorBuilder builder) {
-		return builder.routes().build();
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 }
